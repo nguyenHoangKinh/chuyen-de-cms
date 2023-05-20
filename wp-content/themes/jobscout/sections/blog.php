0aeb31737184bf7861802276dd4dd334b6a96714
nguyenHoangKinh <hoangkinh.tdc2223@gmail.com>
2023-05-20 09:08:26 +0700

    .

--- wp-content/themes/jobscout/sections/blog.php
+++ wp-content/themes/jobscout/sections/blog.php
@@ -24,7 +24,7 @@ $args = array(
 $qry = new WP_Query($args);
 
 if ($ed_blog && ($blog_heading || $sub_title || $qry->have_posts())) { ?>
-    <section id="blog-section" class="article-section bg-st-edit_nghiem" style="background: #f5f5f7;">
+    <section id="blog-section" class="article-section bg-st-edit" style="background: #f5f5f7;">
         <div class="container">
             <?php
             if ($blog_heading) echo '<h2 class="section-title">' . esc_html($blog_heading) . '</h2>';
@@ -32,32 +32,32 @@ if ($ed_blog && ($blog_heading || $sub_title || $qry->have_posts())) { ?>
             ?>
 
             <?php if ($qry->have_posts()) { ?>
-                <div class="article-wrap bonus-edit_nghiem">
+                <div class="article-wrap bonus-edit">
                     <?php
                     while ($qry->have_posts()) {
                         $qry->the_post(); ?>
-                        <div class="content_my_box_nghiem">
-                            <div class="content_my_box_left_nghiem">
+                        <div class="content_my_box">
+                            <div class="content_my_box_left">
 
                                 <?php
                                 if (has_post_thumbnail()) {
-                                    echo get_the_post_thumbnail($post->ID, 'post-thumbnail', "class=img-fluid edit_anh_nghiem");
+                                    echo get_the_post_thumbnail($post->ID, 'post-thumbnail', "class=img-fluid edit_anh");
                                 } else {
                                     jobscout_fallback_svg_image('jobscout-blog');
                                 }
                                 ?>
                             </div>
-                            <div class="content_my_box_right_nghiem">
-                                <div class="header_content_box_nghiem">
-                                    <h4 class="header_content_box_child_nghiem">
-                                        <a href="<?php the_permalink(); ?>" class="header_text_content_box_nghiem"><?php the_title(); ?></a>
+                            <div class="content_my_box_right">
+                                <div class="header_content_box">
+                                    <h4 class="header_content_box_child">
+                                        <a href="<?php the_permalink(); ?>" class="header_text_content_box"><?php the_title(); ?></a>
                             </h4>
                                 </div>
-                                <div class="center_content_box_nghiem">
+                                <div class="center_content_box">
                                     <?php the_content()?>
                                 </div>
-                                <div class="readmore_content_box_nghiem">
-                                <a href="<?php the_permalink(); ?>" class="readmore_text_nghiem">Read more</a>
+                                <div class="readmore_content_box">
+                                <a href="<?php the_permalink(); ?>" class="readmore_text">Read more</a>
                                 </div>
                             </div>
                         </div>
@@ -66,11 +66,6 @@ if ($ed_blog && ($blog_heading || $sub_title || $qry->have_posts())) { ?>
                     wp_reset_postdata();
                     ?>
                 </div><!-- .article-wrap -->
-                <?php if( $blog && $label ){ ?>
-                <div class="btn-wrap">
-        			<a href="<?php the_permalink( $blog ); ?>" class="btn"><?php echo esc_html( $label ); ?></a>
-        		</div>
-            <?php } ?>
             <?php } ?>
         </div>
     </section>
